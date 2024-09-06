621214ad0e0fbcdffb4e2c8860d4a77419c9f321
Dev-Bhargava <95955218+Dev-Bhargava@users.noreply.github.com>
2024-09-07 02:33:32 +0530

    Working On Multilingual

--- assets/js/main.js
+++ assets/js/main.js
@@ -436,3 +436,14 @@
 
 
 })(jQuery, window)
+
+// Multilingual Select Box
+$(".default_option").click(function(){
+    $(this).parent().toggleClass("active");
+})
+
+$(".select_ul li").click(function(){
+    var currentele = $(this).html();
+    $(".default_option li").html(currentele);
+    $(this).parents(".select_wrap").removeClass("active");
+})
